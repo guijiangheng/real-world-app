21ce903fc8f808d188b8d1096d3bae1dad15eaec
桂江亨 <guijiangheng@bytedance.com>
2021-12-16 20:09:30 +0800

    feat: tsconfig add alias

---
+++ test/jest-e2e.js
@@ -0,0 +1,15 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('../tsconfig');
+
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.e2e-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/../src/',
+  }),
+};

4fbf95b93418cadc122be07bd25e4d53d1b3ddde
桂江亨 <guijiangheng@bytedance.com>
2021-12-16 20:29:52 +0800

    feat: add swagger documtation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiProduces } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
 
@@ -7,6 +8,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Hello world',
+    description: 'Hello world API for health check',
+  })
+  @ApiProduces('text/plain')
+  @ApiOkResponse({ description: 'Server works fine' })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Real World App')
+      .setDescription('Real world app REST APIs')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(3000);
 }
+
 bootstrap();
